f40b699f06e7c839d1154c07e94637ee7754e9ea
Ankur Sharma <inbox.ankur@pm.me>
2026-03-04 20:04:00 +0100

    Add .config/nvim/lua/plugins/diffview.lua

---
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,47 @@
+-- Enhanced Git diff viewing with diffview.nvim
+return {
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+      "DiffviewFileHistory"
+    },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+      { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory<cr>", desc = "File History" },
+      { "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "Current File History" },
+    },
+    opts = {
+      enhanced_diff_hl = true,  -- Better syntax highlighting
+      view = {
+        default = {
+          layout = "diff2_horizontal",  -- or "diff2_vertical"
+        },
+        merge_tool = {
+          layout = "diff3_horizontal",
+          disable_diagnostics = true,
+        },
+      },
+      file_panel = {
+        listing_style = "tree",  -- or "list"
+        tree_options = {
+          flatten_dirs = true,
+          folder_statuses = "only_folded",
+        },
+      },
+      -- Catppuccin theme integration
+      hooks = {
+        diff_buf_read = function(bufnr)
+          vim.opt_local.wrap = false
+          vim.opt_local.list = false
+          vim.opt_local.colorcolumn = "80"
+        end,
+      },
+    },
+  },
+}
