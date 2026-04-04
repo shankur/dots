542449a7af114c508f14ad8b7f872b50e6082f7c
Ankur Sharma <inbox.ankur@outlook.com>
2026-04-04 21:43:17 +0200

    Add F1 keybinding to open Claude Code in Zellij floating pane

--- dot_config/nvim/lua/polish.lua
+++ dot_config/nvim/lua/polish.lua
@@ -2,6 +2,11 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+-- Open Claude Code in a Zellij floating pane
+vim.keymap.set('n', '<F1>', function()
+  vim.fn.system('zellij action new-pane --floating -- claude')
+end, { desc = 'Open Claude Code' })
+
 -- Disable the intro/welcome screen
 vim.opt.shortmess:append("I")
 
